a7c7f1b4f93e6dbca1bf888102bb7626465219a9
xromo <xromo@DESKTOP-J6CQG2O>
2025-06-23 18:39:24 +0300

    string DONE

--- NULLTerminatedLines/main.cpp
+++ NULLTerminatedLines/main.cpp
@@ -8,21 +8,25 @@ void ToLower(char* str);
 void Shrink(char* str);
 bool is_palindrome(const char* str);
 bool is_int_number(const char* str);
-int to_int_number(char* str);
+int to_int_number(const char* str);
 bool is_bin_number(const char* str);
+int bin_to_dec(const char* str);
+bool is_hex_number(const char* str);
+int hex_to_dec(const char* str);
 
 void main()
 {
-	setlocale(LC_ALL, "");
-	//char str[] = { 'H', 'e', 'l', 'l', 'o', 0 };
-	//char str[] = "Hello";
-	const int SIZE = 20;
-	char str[SIZE] = {};
-	cout << "Hello world!" << endl;
-	SetConsoleCP(1251);
-	cin.getline(str, SIZE);
-	SetConsoleCP(866);
-	cout << StrLength(str);
+	//setlocale(LC_ALL, "");
+	////char str[] = { 'H', 'e', 'l', 'l', 'o', 0 };
+	////char str[] = "Hello";
+	//const int SIZE = 20;
+	//char str[SIZE] = {};
+	//cout << "Hello world!" << endl;
+	//SetConsoleCP(1251);
+	//cin.getline(str, SIZE);
+	//SetConsoleCP(866);
+	//cout << StrLength(str);
+
 }
 
 int StrLength(const char* str)
@@ -37,7 +41,7 @@ void ToUpper(char* str)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			str[i] -= 20;
+			str[i] -= 32;
 		}
 	}
 }
@@ -47,7 +51,7 @@ void ToLower(char* str)
 	{
 		if (str[i] >= 'A' && str[i] <= 'Z')
 		{
-			str[i] += 20;
+			str[i] += 32;
 		}
 	}
 }
@@ -82,13 +86,13 @@ bool is_palindrome(const char* str)
 	for (int i = 0; i < len; i++)
 	{
 		char element = str[i];
-		if (str[i] >= 'A' && str[i] <= 'Z') element += 20;
+		if (str[i] >= 'A' && str[i] <= 'Z') element += 32;
 		if ((element >= 'a' && element <= 'z') || (element >= '0' && element <= '9'))
 		{
 			filtered[index++] = element;
 		}
 	}
-	for (int i = 0; i < index; i++)
+	for (int i = 0; i < index/2; i++)
 	{
 		if (filtered[i] != filtered[index - i - 1])
 		{
@@ -107,7 +111,7 @@ bool is_int_number(const char* str)
 	}
 	return true;
 }
-int to_int_number(char* str)
+int to_int_number(const char* str)
 {
 	int i = 0, num = 0, sign = 1;
 	for (; str[i] != '0'; i++)
@@ -127,4 +131,37 @@ bool is_bin_number(const char* str)
 		}
 	}
 	return true;
+}
+int bin_to_dec(const char* str)
+{
+	int rezult = 0;
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		rezult = rezult * 2 + (str[i] - '0');
+	}
+	return rezult;
+}
+bool is_hex_number(const char* str)
+{
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (!((str[i] >= '0' && str[i] <= '9') || (str[i] >= 'a' && str[i] <= 'f') || (str[i] >= 'A' && str[i] <= 'F')))
+		{
+			return false;
+		}
+	}
+}
+int hex_to_dec(const char* str)
+{
+	int rezult = 0, i = 0;
+	while (str[i] != '\0')
+	{
+		int digit;
+		if (str[i] >= '0' && str[i] <= '9') digit = str[i] - '0';
+		else if (str[i] >= 'a' && str[i] <= 'z') digit = str[i] - 'a';
+		else if (str[i] >= 'A' && str[i] <= 'Z') digit = str[i] - 'A';
+		else digit = 0;
+		rezult = rezult * 16 + digit;
+	}
+	return rezult;
 }
